e5205df2d8e3f19ea52c9fcae1f7723b5281fae1
techedu18 <113489375+techedu18@users.noreply.github.com>
2022-09-14 17:38:13 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,19 +1,33 @@
-let COUNT = 0
 input.onButtonPressed(Button.A, function () {
-	
-})
-input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 4; index++) {
-        music.playTone(523, music.beat(BeatFraction.Breve))
-        basic.showString("STOP!")
-    }
-})
-input.onGesture(Gesture.EightG, function () {
-    for (let index = 0; index < 4; index++) {
-        music.playTone(988, music.beat(BeatFraction.Breve))
-    }
-    basic.showString("Fast!")
+    music.playTone(659, music.beat(BeatFraction.Double))
+    basic.showString("START")
+    basic.pause(COUNT * 60000)
+    music.playTone(659, music.beat(BeatFraction.Double))
+    music.playTone(659, music.beat(BeatFraction.Double))
 })
 input.onButtonPressed(Button.AB, function () {
     COUNT = 0
+    basic.showString("RESET")
 })
+input.onButtonPressed(Button.B, function () {
+    COUNT += 1
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.showNumber(COUNT)
+    basic.showString("MIN")
+})
+let COUNT = 0
+basic.showString("Hello!")
+COUNT = 0
+for (let index = 0; index < 2; index++) {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+}
+basic.showLeds(`
+    . # . # .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
